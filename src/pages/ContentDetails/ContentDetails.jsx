6784ca071a9a2d1bcc6fdebf2a42e40cf6a0eef9
constantino pasquali <conipasquali2@gmail.com>
2025-04-10 10:56:04 -0300

    edicion del responsive en mobile

--- src/pages/ContentDetails/ContentDetails.jsx
+++ src/pages/ContentDetails/ContentDetails.jsx
@@ -26,16 +26,39 @@ export const ContentDetails = ({ type }) => {
   }, []);
 
   return (
-    <section className="flex mt-20 items-center bg-red-50 justify-center gap-6">
-      <img
-        className="h-102"
-        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-        alt={`poster de ${data.title}`}
-      />
-      <div className="flex flex-col gap-6 self-start mt-2">
-        <h2 className="font-medium text-xl">{title}</h2>
-        <p className="w-60">{data.overview}</p>
-      </div>
-    </section>
+    <>
+      <section
+        className="details-container flex mt-16 items-center bg-red-50 justify-center gap-6 relative max-md:flex-col pb-4 py-4"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${data.backdrop_path})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+        }}
+      >
+        <img
+          className="h-102 z-10"
+          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt={`poster de ${data.title}`}
+        />
+        <div className="flex flex-col items-center gap-6 mt-2 z-10">
+          <h2 className="font-medium text-xl text-white">{title}</h2>
+          <p className="w-120 max-md:w-70  text-white">{data.overview}</p>
+        </div>
+        <div className="absolute bottom-0 left-0 w-full h-42 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+      </section>
+      <section className="flex flex-wrap w-full items-center justify-center gap-10">
+        {data.production_companies?.map((company) => {
+          return (
+            <article key={company.id}>
+              <img
+                className="h-32 w-32 object-contain"
+                src={`https://image.tmdb.org/t/p/w500/${company.logo_path}`}
+                alt={`logo de compania ${company.name}`}
+              />
+            </article>
+          );
+        })}
+      </section>
+    </>
   );
 };
