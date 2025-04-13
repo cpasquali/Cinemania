76d1bb78f7c23850f234a80fa8072d503d474cf4
cpasquali <conipasquali2@gmail.com>
2025-04-13 18:31:41 -0300

    se agrego un favicon y logo en la nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -49,7 +49,7 @@ export const NavBar = ({ type, setType, searchMovie, setSearchMovie }) => {
             <div className="flex items-center">
               <div className="shrink-0">
                 <img
-                  src="../../.././logo.png"
+                  src="../../.././public/logo.png"
                   alt="logo de la pagina"
                   className="image-logo w-25 mt-2 object-contain"
                 />
